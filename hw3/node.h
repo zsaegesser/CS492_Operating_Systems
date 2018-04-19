48bd739e0f3c53fc6a7ab5d565176301f072900b
zachsaegesser <zachsaegesser@gmail.com>
2018-04-19 19:39:15 -0400

    added basic node.h file

--- hw3/node.h
+++ hw3/node.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <time>
 
 using namespace std;
 
@@ -7,8 +8,26 @@ class Node {
 public:
   char * name;
   int size;
-  int timestamp; //should probably be an actual timestamp (time_val)
+  time_t timestamp;
   Node parent;
-  Node[] children;
+  std::vector<Node> children;
   bool type; //0 for directory, 1 for file
+
+  Node(char * node_name, int node_size, time_t node_timestamp){
+    this.name = node_name;
+    this.size = node_size;
+    this.timestamp = node_timestamp;
+  }
+
+  void set_parent(Node node_parent){
+    this.parent = node_parent;
+  }
+
+  void add_child(Node new_node_child){
+    this.children.push_back(new_node_child);
+  }
+
+  // void remove_child(char * node_name){
+  //   //remove the child with name node_name
+  // }
 }
