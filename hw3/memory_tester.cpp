9683868d7007d51f64df2f8eff9af4bd0cd2d58e
zachsaegesser <zachsaegesser@gmail.com>
2018-04-24 17:06:30 -0400

    adjusted lfile.h remove and add functions, modified mem tester

--- hw3/memory_tester.cpp
+++ hw3/memory_tester.cpp
@@ -7,17 +7,54 @@
 #include <cstdlib>
 #include <cstring>
 #include "ldisk.h"
+#include "lfile.h"
 
 using namespace std;
 
 int main(int argc, char * const argv[]){
-  Ldisk * t = new Ldisk(100,10);
+  Ldisk * d = new Ldisk(100,10); // (disk_size, block_size)
+  Lfile * f = new Lfile(0, NULL, 10);
+  cout << "initialize" <<endl << flush;
+  d->print();
+  f->print_lfile();
 
+  f->add_bytes(100, d);
+
+  cout << "Added" <<  endl << flush;
+  d->print();
+  f->print_lfile();
+
+  // f->remove_bytes(5, d);
+  f->remove_bytes(90, d);
+
+  // f->add_bytes(100, d);
+  // f->add_bytes(5,d);
+  // f->add_bytes(6,d);
+  cout << " Removed" << endl << flush;
+  d->print();
+  f->print_lfile();
+
+
+
+
+
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
+  // cout << "Mem Address Fetch: " << t->fetch() << endl << flush;
   // block_group * new_3 = new block_group(0,pair<int,int> (8,9),NULL);
   // block_group * new_2 = new block_group(1,pair<int,int> (4,7),new_3);
   // block_group * new_1 = new block_group(0,pair<int,int> (0,3),new_2);
   // t->head = new_1;
   //t->print();
-  t->remove(70);
-  t->print();
+  // t->remove(70);
+  // t->print();
 }
